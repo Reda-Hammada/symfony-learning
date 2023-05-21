6137c22b562f4c031f26a03629037e272b4c9980
Reda Hammada <hammada.reda.dev@gmail.com>
2023-05-21 17:38:36 +0100

    twig template filiter & functions example

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -7,9 +7,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HelloController extends AbstractController
 {    
-
-
-    private $message = ['Hello','Hallo','Bonjour','Holla'];
+    private array $message = [
+      [ 'message' => 'Hello', 'created'=> '2022/06/12'],
+      ['message' =>  'Hallo', 'created'=> '2022/04/12'],
+      ['message' =>  'Bonjour', 'created'=> '2022/05/12'],
+    ];
 
     #[Route('/home',name: 'home')]
     public function index(): Response
